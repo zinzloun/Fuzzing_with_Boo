ae09fe370a842404cfa4c98ff01b19a45a3566e6
Filippo *ZinzlouN* Bersani <filobers@yahoo.it>
2020-04-27 22:45:52 +0200

    Update bFuzzPCManFTP.py

--- bFuzzPCManFTP.py
+++ bFuzzPCManFTP.py
@@ -5,18 +5,18 @@
 
 from boofuzz import *
 
-host = '192.168.1.6'    #Host IP
-port = 21           #Server port
+host = '192.168.1.6'    # Host IP
+port = 21               # Server port
 
 def main():
  # setting the restart threshold we will stop fuzzing after 5 attempts without response
  ses = Session(target = Target(connection = SocketConnection(host, port, proto='tcp')),restart_threshold = 5)
- s_initialize("PCMan_FTPS")           #just givin a session name
+ s_initialize("PCMan_FTPS")        # the session name
  
  s_string("FUZZ")                  # please holder for the fuzzing string
  
- ses.connect(s_get("PCMan_FTPS")) #having our 'session' connected
- ses.fuzz()                 #calling this function actually performs the fuzzing
+ ses.connect(s_get("PCMan_FTPS")) # having our 'session' connected
+ ses.fuzz()                       # calling this function actually performs the fuzzing
 
 if __name__ == "__main__":
     main()
